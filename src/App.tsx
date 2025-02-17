c6b36111f0693227ad70199752c2cac1368a5ae2
orkarfabianthewise <orkarmelch@gmail.com>
2025-02-17 12:56:46 +0100

    wallet connect, component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   Navigate,
+// } from "react-router-dom";
 import Home from "./components/Home";
-import User from "./components/User";
+// import User from "./components/User";
 import WalletContextProvider from "./components/walletConnect";
 
 const App: React.FC = () => {
